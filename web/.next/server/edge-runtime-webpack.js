468e126c2e62daebe69f2ab05b9cdcec631d5e46
FarhanSayed16 <farhansayed54@gmail.com>
2026-08-23 23:41:36 +0530

    feat: implement core platform modules, landing page components, and auth/API utility services

--- web/.next/server/edge-runtime-webpack.js
+++ web/.next/server/edge-runtime-webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("45361c457222a221")
+/******/ 		__webpack_require__.h = () => ("a252556f480d835e")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
